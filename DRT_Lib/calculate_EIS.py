49802a94bd237d2002d63983c7ed1c134844f5ae
Giang iMac <giangtle@uw.edu>
2020-06-18 12:43:42 -0700

    correct error

--- DRT_Lib/calculate_EIS.py
+++ DRT_Lib/calculate_EIS.py
@@ -4,6 +4,6 @@ from .submodule import calc_A_re, calc_A_im
 
 def calculate_EIS(freq_vec, gamma, R_inf):
     A_re = calc_A_re(freq_vec)
-    A_im = calc_A_re(freq_vec)
+    A_im = calc_A_im(freq_vec)
     Z_cal = R_inf + np.matmul(A_re, gamma) + 1j*np.matmul(A_im, gamma)
     return Z_cal
